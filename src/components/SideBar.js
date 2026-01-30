907ef888bd94897eb423d152e5076fa06f9a5e1d
admin <bugsteam9912@gmail.com>
2026-01-30 16:27:10 +0900

    즐겨찾기 버튼 수정

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -17,18 +17,17 @@ const IconHome = () => (
     <polyline points="9 22 9 12 15 12 15 22"></polyline>
   </svg>
 );
-const IconStar = () => (
-  <svg
-    width="20"
-    height="20"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-  >
-    <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon>
+const IconStar = ({ filled = false, color = "currentColor" }) => (
+  <svg width="20" height="20" viewBox="0 0 24 24">
+    <polygon
+      points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"
+      fill={filled ? color : "none"}
+      stroke={color}
+      strokeWidth="2"
+    />
   </svg>
 );
+
 const IconChevronLeft = () => (
   <svg
     width="24"
@@ -495,13 +494,9 @@ export default function SideBar() {
         <TopBar>
           <TopBarLeft>
             <PaddedIconBtn onClick={toggleCurrentPin}>
-              <IconStar
-                style={{
-                  color: isPinned ? "#FFD60A" : "var(--font)",
-                  fill: isPinned ? "#FFD60A" : "none",
-                }}
-              />
+              <IconStar filled={isPinned} color="var(--main)" />
             </PaddedIconBtn>
+
             <Divider />
             <PaddedIconBtn onClick={() => navigate("/")}>
               <IconHome />
