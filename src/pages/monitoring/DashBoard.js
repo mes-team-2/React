f27740bc41dd5c051b70aa11acaa05a611f38c3e
admin <bugsteam9912@gmail.com>
2026-01-30 15:08:53 +0900

    대시보드 파이 차트 색상 수정

--- src/pages/monitoring/DashBoard.js
+++ src/pages/monitoring/DashBoard.js
@@ -68,7 +68,7 @@ const DEFECT_TYPE_CHART = [
   { name: "기타", value: 10 },
 ];
 
-const PIE_COLORS = ["#FF5B5B", "#FF9F0A", "#FFD60A", "#34C759", "#AF52DE"];
+const PIE_COLORS = ["#FF5B5B", "#FF9F0A", "#938bff", "#34C759", "#AF52DE"];
 
 const MACHINE_ENV = [
   {
